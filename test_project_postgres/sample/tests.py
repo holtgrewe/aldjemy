cae18f67810d61042d15ffe7107ef90871f5da34
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2021-08-31 11:06:34 +0200

    WIP: expose inconsistency with JSON handling

--- test_project_postgres/sample/tests.py
+++ test_project_postgres/sample/tests.py
@@ -94,6 +94,24 @@ class TestJsonField(TestCase):
         """
         assert JsonModel.sa is not None
 
+    def test_query_django(self):
+        stor = JsonModel(value={"key": "value"})
+        stor.save()
+
+        obj = JsonModel.objects.get(id=stor.id)
+        self.assertEquals(obj.value, {"key": "value"})
+
+    def test_query_sa(self):
+        stor = JsonModel(value={"key": "value"})
+        stor.save()
+
+        query = select([JsonModel.sa.id, JsonModel.sa.value])
+        with get_engine().begin() as connection:
+            test_data = connection.execute(query)
+
+        obj = list(test_data)[0]
+        self.assertEquals(obj.value, {"key": "value"})
+
 
 class TestDateRangeField(TestCase):
     def test_model_creates(self):
